fcf17153ad1567c236e227fcda8e0f597ce1d4f3
buckfast <owwbua@gmail.com>
2018-04-24 13:01:21 +0300

    game room list

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -77,9 +77,6 @@ const run = () => {
   }, false);
 }
 
-const banner = () => {
-
-}
 
 const drawCountdown = (count) => {
   contextui.clearRect(0, 0, canvasui.width, canvasui.height);
@@ -140,7 +137,7 @@ const setCurrentPlaceable = (wallBlock) => {
 
 const banner = () => {
   contextfg.clearRect(0, 0, canvasfg.width, canvasfg.height);
-  // 
+  //
   // const FPS = 30;
   // setInterval(() => {
   //
